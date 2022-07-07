e0dc23599f137bfa6dc1d603ab263ef8ee382086
yusragcelikel <74064968+yusragcelikel@users.noreply.github.com>
2022-07-07 16:38:15 +0300

    Anonymous veri tipi

---
+++ C#Dersleri/AnonymousVeriTipi/AnonymousVeriTipi/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+/*
+ * 
+ * C#'ta anonim tipi, yalnızca public salt okunur özellikleri içerebilen
+ * herhangi bir adı olmayan bir veri tipidir (sınıf).
+ * 
+*/
+
+
+namespace AnonymousVeriTipi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var student = new { Id = 1, FirstName = "James", LastName = "Bond" };
+
+            Console.WriteLine(student.Id); // Id'nin veri tipi int
+            Console.WriteLine(student.FirstName); // FirstName'in veri tipi string
+            Console.WriteLine(student.LastName);  // LastName'in veri tipi string
+
+            /*
+             * 
+             *  Özellikler, nesne özelliklerinde olduğu gibi nokta (.) 
+             *  gösterimi kullanılarak erişilebilir. Ancak, salt okunur 
+             *  oldukları için değişkenlerin değerlerini değiştiremezsiniz.
+             * 
+            */
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
