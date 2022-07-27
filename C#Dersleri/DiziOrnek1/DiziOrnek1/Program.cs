392d8387368caba420c67ef31aa01fe243f45c77
Yusra Celikel <yusragcelikel@gmail.com>
2022-07-27 18:47:14 +0300

    27July2022

---
+++ C#Dersleri/DiziOrnek1/DiziOrnek1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiziOrnek1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan bir sayı alınır.
+            // Bu sayı tanımlı dizi içerisinde aratılır.
+            // Eğer dizi içinde varsa kaç tane olduğunu,
+            // Yoksa da "yoktur" yazan program:
+
+            int[] ints = { 10, 10, 20, 30, 20, 30, 40, 50, 40, 50, 70, 55, 65, 70, 55, 65, 5, 20, 35, 65, 5, 20, 35, 65, 80 };
+            int count = 0;
+
+            Console.WriteLine("Lütfen bir sayı giriniz: ");
+            int alinan_sayi = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+                if (ints[i] == alinan_sayi )
+                {
+
+                    count = count + 1;
+
+                }
+
+            }
+            if (count == 0)
+            {
+                Console.WriteLine("Girdiğiniz sayı tanımlı dizide bulunmamaktadır.");
+
+            }
+            
+            else
+            {
+                Console.WriteLine("Girdiğiniz sayıdan " + count + " tane vardır.");
+            }
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ C#Dersleri/DizilerOrnek2/DizilerOrnek2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DizilerOrnek2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kullanıcıdan 10 tane int veri tipine sahip sayı alınır.
+            // Kaç adet çift sayı, kaç adet tek sayı olduğunu belirten,
+            // Çift değerlerin toplamını, tek değerlerin topamını belirten;
+            // Çift değerlerin toplamı tek değerlerin toplamından büyükse belirtsin.
+            // Tek değerlerin toplamı çift değerlerin toplamından büyükse belirtsin.
+
+            int even_count = 0;
+            int odd_count = 0;
+            int even_add = 0;
+            int odd_add = 0;
+            int alinan_sayi;
+            
+            int[] ints = new int[10];
+
+
+            for (int i = 0; i < ints.Length; i++)
+            {
+
+                Console.Write ("Lütfen dizinin " + i + ". index tam sayı değerini giriniz: ");
+                alinan_sayi = Convert.ToInt32(Console.ReadLine());
+
+            }
+            
+            Console.ReadLine();
+            
+
+
+
+
+            // to be continued
+
+
+
+
+        }
+    }
+}
